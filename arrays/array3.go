a0cc6d166107667b88a0b90a161bfcd4f0515b16
MachariaP <walburphinehas78@gmail.com>
2024-11-20 13:30:02 +0300

    Accessing the first and third elements in the prices array

---
+++ arrays/array3.go
@@ -0,0 +1,12 @@
+// How to access the first snd third elements in the prices array
+
+package main
+
+import "fmt"
+
+func main() {
+	prices := [5]int{10, 20, 30, 40, 50}
+
+	fmt.Println(prices[0]) // Output: 10
+	fmt.Println(prices[2]) // Output: 30
+}
