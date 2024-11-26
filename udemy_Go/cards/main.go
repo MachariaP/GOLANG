38fe5d13f224ff94630b3ec068b83b06f1b444f9
MachariaP <walburphinehas78@gmail.com>
2024-11-26 14:43:17 +0300

    Implement slice manipulation and card iteration using Go
    - Created a slice to store a collection of card strings.
    - Used the `append` function to add a new card (`Six of Spades`) to the slice.
    - Added a `for` loop with the `range` function to iterate over the slice and print each card with its index.
    - Included a `newCard` function that returns a card string (`Five of Diamonds`) to demonstrate modularity.

--- udemy_Go/cards/main.go
+++ udemy_Go/cards/main.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
+// main is the entry point of the program
 func main() {
-	card := newCard()
+	// create a slice of strings named "cards"
+	// initialize it with "Ace of Diamonds
+	// and the result of newCard function
+	cards := []string{"Ace of Diamonds", newCard()}
 
-	fmt.Println(card)
+	// append "Six of Spade" to the slice "cards"
+	cards = append(cards, "Six of Spades")
+
+	// Loop over the slice "cards", using range to get both index(i) 
+	// and value (card)
+	for i, card := range cards {
+
+		// print the index and the card value
+		fmt.Println(i, card)
+	}
 }
 
+// newCard returns a string representing a card.
 func newCard() string  {
 	return "Five of Diamonds"
 }
