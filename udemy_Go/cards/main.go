327c63079d91f1150f6c99250cb69c3b7e425400
MachariaP <walburphinehas78@gmail.com>
2024-11-26 16:21:42 +0300

    Learned and implemented receiver methods in Go
    - Introduced a custom type `deck` as a slice of strings to represent a deck of cards.
    - Implemented a receiver method `print()` for the `deck` type to iterate and print each card with its index.
    - Updated the `main` function to use the new `deck` type and its `print()` method for enhanced modularity and readability.

--- udemy_Go/cards/main.go
+++ udemy_Go/cards/main.go
@@ -1,24 +1,17 @@
 package main
 
-import "fmt"
-
 // main is the entry point of the program
 func main() {
 	// create a slice of strings named "cards"
 	// initialize it with "Ace of Diamonds
 	// and the result of newCard function
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 
 	// append "Six of Spade" to the slice "cards"
 	cards = append(cards, "Six of Spades")
 
-	// Loop over the slice "cards", using range to get both index(i) 
-	// and value (card)
-	for i, card := range cards {
+	cards.print()
 
-		// print the index and the card value
-		fmt.Println(i, card)
-	}
 }
 
 // newCard returns a string representing a card.
