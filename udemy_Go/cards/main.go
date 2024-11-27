64d70f5100df6f132c0b15bbefc7992ea6896ba0
MachariaP <walburphinehas78@gmail.com>
2024-11-27 10:15:30 +0300

    Added a `newDeck` function to generate and print a full deck of cards
    - Implemented the `newDeck` function to create a new deck by combining card values (`Ace`, `Two`, etc.) with suits (`Spades`, `Diamonds`, etc.).
    - Used nested loops to construct all possible card combinations and append them to the `deck` slice.
    - Updated the `main` function to call `newDeck` and use the `print` method to display the generated cards.

--- udemy_Go/cards/main.go
+++ udemy_Go/cards/main.go
@@ -1,20 +1,7 @@
 package main
 
-// main is the entry point of the program
 func main() {
-	// create a slice of strings named "cards"
-	// initialize it with "Ace of Diamonds
-	// and the result of newCard function
-	cards := deck{"Ace of Diamonds", newCard()}
-
-	// append "Six of Spade" to the slice "cards"
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-
-}
-
-// newCard returns a string representing a card.
-func newCard() string  {
-	return "Five of Diamonds"
 }
